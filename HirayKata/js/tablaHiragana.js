6e21488a4c53d6b0c16de29f7f53bac56743c656
heraces <david.ballesta@hotmail.com>
2022-09-02 17:08:36 +0200

    por lo menos ahora muestra algo

--- HirayKata/js/tablaHiragana.js
+++ HirayKata/js/tablaHiragana.js
@@ -1,25 +1,35 @@
-window.onload = fill_table()
+window.onload = start()
 
 
 
 function fill_it(){
 
-    table = document.getElementById("hiragana_table").getElementsByTagName("tbody")[0]
     for(let i = 0; i<len(window.esp);i++){
         table.append(i)
     }
+            
+    for(let s=0; s<window.esp.length; i++){
+        console.log(window.esp[s])
+    }
 }
-function fill_table(){
-    Papa.parse(("../kanas/hiragana.csv"),{download:true, headers: false, 
-        delimiter: ',',skipEmptyLines: true, errors: function(err){
-            console.log(err)
-            return false
-        },
-        complete: function(results, error){
-        console.log(error)
-        console.log(results.data)
-        window.hiragana = results.data} })
-        
+function start(){
+    table = document.getElementById("hiragana_table").getElementsByTagName("tbody")[0]
+    Papa.parse(("../../kanas/hiragana.csv"),{download:true, headers: false, 
+        delimiter: ',',skipEmptyLines: true, complete: function(results){
+            for(let i = 0; i<results.data.length;i++){
+                let x = document.createElement("tr")
+                for(let s = 0; s<results.data[i].length;s++){
+                    x.append(results.data[i][s])
+                }
+                table.append(x)
+            }
+            hiragana = results.data}
+        }, )
+    Papa.parse(("../../kanas/esp-kana.csv"),{download:true, headers: false, 
+        delimiter: ',',skipEmptyLines: true, complete: function(results){
+            window.esp = results.data}
+        }, )
+
 }
 
 
